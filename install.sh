a742d8fa4031ee0e7463334e8bc638c6d645a3ac
kaizhu_Qin <330428607@qq.com>
2018-05-13 00:07:23 +0800

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,6 @@
+echo "# centos-install-google-authenticator" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git remote add origin https://github.com/kaizhuQin/centos-install-google-authenticator.git
+git push -u origin master
